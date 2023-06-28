6ac155ccd01b44617cebfffa4513776123eb8fe8
Petersil1998 <silbernagl.peter@rolmail.net>
2023-06-28 18:25:19 +0200

    Implemented Caching

--- src/main/java/net/petersil98/core/http/HTTPClient.java
+++ src/main/java/net/petersil98/core/http/HTTPClient.java
@@ -1,6 +1,9 @@
 package net.petersil98.core.http;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import net.petersil98.core.Core;
+import net.petersil98.core.util.settings.Settings;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.logging.log4j.Marker;
@@ -12,6 +15,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +23,7 @@ import java.util.Map;
 public class HTTPClient {
 
     private final HttpClient client;
+    private final static Cache<String, HttpResponse<String>> CACHE = CacheBuilder.newBuilder().maximumSize(1000).expireAfterAccess(Duration.ofMinutes(10)).build();
     private static final HTTPClient INSTANCE = new HTTPClient();
     private static final Marker MARKER = MarkerManager.getMarker(HTTPClient.class.getSimpleName());
 
@@ -31,9 +36,16 @@ public class HTTPClient {
     }
 
     public HttpResponse<String> get(String url, Map<String, String> params) {
+        String fullUrl = url+"?"+buildParameters(params);
+        if(Settings.shouldCache()) {
+            HttpResponse<String> cachedResponse = CACHE.getIfPresent(fullUrl);
+            if (cachedResponse != null) return cachedResponse;
+        }
         try {
-            URI uri = URI.create(url+"?"+buildParameters(params));
-            return request(HttpRequest.newBuilder(uri).GET().build());
+            URI uri = URI.create(fullUrl);
+            HttpResponse<String> response = request(HttpRequest.newBuilder(uri).GET().build());
+            if(Settings.shouldCache() && response.statusCode() >= 200 && response.statusCode() < 300) CACHE.put(fullUrl, response);
+            return response;
         } catch (Exception e) {
             Core.LOGGER.error(MARKER, "Failed to perform GET request", e);
         }
