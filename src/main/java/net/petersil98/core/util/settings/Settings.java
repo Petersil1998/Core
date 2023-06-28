6112abb96110967b9f9cd47191be8cfe2439aab7
Petersil1998 <silbernagl.peter@rolmail.net>
2023-06-28 19:03:17 +0200

    Implemented Caching

--- src/main/java/net/petersil98/core/util/settings/Settings.java
+++ src/main/java/net/petersil98/core/util/settings/Settings.java
@@ -1,5 +1,12 @@
 package net.petersil98.core.util.settings;
 
+import net.petersil98.core.util.Loader;
+
+import java.time.Duration;
+import java.time.Period;
+import java.time.temporal.TemporalUnit;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -8,7 +15,8 @@ public class Settings {
     private static ServerConfig currentConfig = ServerConfig.EUW_CONFIG;
     private static Language language = Language.EN_US;
     private static Supplier<String> encryptedKey;
-    private static Function<String, String> decryptor;
+    private static Function<String, String> decryptor = (s -> s);
+    private static boolean shouldCache = true;
 
     public static ServerConfig getCurrentConfig() {
         return Settings.currentConfig;
@@ -36,5 +44,13 @@ public class Settings {
 
     public static void setDecryptor(Function<String, String> decryptor) {
         if (decryptor != null) Settings.decryptor = decryptor;
+    }
+
+    public static void setShouldCache(boolean shouldCache) {
+        Settings.shouldCache = shouldCache;
+    }
+
+    public static boolean shouldCache() {
+        return shouldCache;
     }
 }
