c8fa90fad74a69cf8fea519bc071e414bf2c0a6b
Petersil1998 <silbernagl.peter@rolmail.net>
2023-07-07 10:08:02 +0200

    Moved responsability to check for new veraions of static data to the Loaders, static data should no longer get stored locally

--- src/main/java/net/petersil98/core/util/Loader.java
+++ src/main/java/net/petersil98/core/util/Loader.java
@@ -1,7 +1,6 @@
 package net.petersil98.core.util;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import net.petersil98.core.constant.Constants;
 import net.petersil98.core.util.settings.Language;
@@ -10,8 +9,11 @@ import org.apache.logging.log4j.core.util.IOUtils;
 
 import java.io.*;
 import java.net.URI;
-import java.net.URL;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static net.petersil98.core.Core.MAPPER;
 
@@ -23,17 +25,26 @@ import static net.petersil98.core.Core.MAPPER;
 public abstract class Loader {
 
     private static final List<Loader> LOADERS = new ArrayList<>();
-
-    private static final String DATA_FOLDER = "data" + File.separator;
-    protected static final String BASE_PATH = System.getProperty("user.dir") + File.separator + DATA_FOLDER;
-
-    private static final String SETTINGS = BASE_PATH + "settings.json";
-    private static final String DDRAGON_VERSION = "ddragonVersion";
-    private static final String LANGUAGE = "language";
+    private static Language latestLanguage;
+    private static final Thread UPDATE_CHECKER = new Thread(() -> {
+        while (true) {
+            if(latestLanguage != Settings.getLanguage()) {
+                latestLanguage = Settings.getLanguage();
+                LOADERS.forEach(Loader::load);
+            } else {
+                LOADERS.stream().filter(Loader::shouldReloadData).forEach(Loader::load);
+            }
+            try {
+                Thread.sleep(Duration.of(30, ChronoUnit.MINUTES));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    });
 
     /**
      * This Method is used to add a Loader, which should get called when {@link #init()} gets called.
-     * Loaders have to extend this class and therefore implement the {@link #load(boolean) load} Method
+     * Loaders have to extend this class and therefore implement the {@link #load} Method
      * @param loader The Loader, which should get called on init
      */
     public static void addLoader(Loader loader) {
@@ -43,78 +54,29 @@ public abstract class Loader {
     /**
      * This Method needs to be called before API calls are being made,
      * but after the settings are set (at least the {@link Settings#setLanguage(Language)})
-     * For this library to work properly. It updates and loads static data
-     * from the Data Dragon and Community Dragon.
+     * For this library to work properly. It updates and loads static data from the Data Dragon and Community Dragon.
+     * This Method also starts a Thread which periodically checks for a new Version of the Data Dragon.
      *
      * @see Settings
      */
     public static void init() {
-        new File(DATA_FOLDER).mkdirs();
-        try {
-            new File(SETTINGS).createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        boolean shouldUpdate = initAndUpdateSettings();
-        LOADERS.forEach(loader -> loader.load(shouldUpdate));
+        latestLanguage = Settings.getLanguage();
+        LOADERS.forEach(Loader::load);
+        if(UPDATE_CHECKER.getState().equals(Thread.State.NEW)) UPDATE_CHECKER.start();
     }
 
     /**
      * This Method gets called on Loaders, which got added by {@link #addLoader(Loader)}.
-     * It should update the data files and load that data into a Collection of Objects.
-     *
-     * @param shouldUpdate Whether the data files should be updated
+     * It should get the latest data load it into a Collection of Objects.
      */
-    protected abstract void load(boolean shouldUpdate);
+    protected abstract void load();
 
     /**
-     * This Method gets the newest Version of the Data Dragon and compares if with the one in the Settings File.
-     * If the Versions don't match or the Language set in {@link Settings} don't match the one in the Settings File <b>true</b> is returned,
+     * This Method gets called on Loaders, which got added by {@link #addLoader(Loader)}.
+     * It should get the newest Version for the static Data and compare it with the latest used Version.
+     * If the Versions don't match or the Language set in {@link Settings} don't match the one in {@link #latestLanguage} <b>true</b> is returned,
      * <b>false</b> otherwise.
-     * <br>
-     * The Settings File is also updated with the new values.
-     * @return A <b>boolean</b> whether the data files should be updated.
+     * @return A <b>boolean</b> whether the Data Collections should be updated.
      */
-    private static boolean initAndUpdateSettings(){
-        String url = Constants.DDRAGON_BASE_PATH + "api/versions.json";
-        try {
-            InputStream in = URI.create(url).toURL().openConnection().getInputStream();
-            String[] versions = MAPPER.readValue(IOUtils.toString(new InputStreamReader(in)), TypeFactory.defaultInstance().constructArrayType(String.class));
-            Constants.DDRAGON_VERSION = versions[0];
-            Map.Entry<Boolean, JsonNode> result = parseSettingsFile();
-            boolean areFieldsSet = result.getKey();
-            JsonNode settings = result.getValue();
-            if(!areFieldsSet || !settings.get(DDRAGON_VERSION).asText().equals(versions[0])
-                    || !settings.get(LANGUAGE).asText().equals(Settings.getLanguage().toString())) {
-                BufferedWriter writer = new BufferedWriter(new FileWriter(SETTINGS));
-                ObjectNode root = MAPPER.createObjectNode();
-                root.put(DDRAGON_VERSION, versions[0]);
-                root.put(LANGUAGE, Settings.getLanguage().toString());
-                writer.write(root.toString());
-                writer.close();
-                return true;
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return false;
-    }
-
-    /**
-     * Utility Method for checking the structure of the settings file.
-     * @return a {@link Map.Entry Pair} consisting of:
-     * <ul>
-     *     <li>A <b>Boolean</b> that indicates, whether the settings file is valid</li>
-     *     <li>A {@link JsonNode} that represents the settings as a Json Object.
-     *     If the Settings File isn't valid, <b>null</b> is returned instead</li>
-     * </ul>
-     */
-    private static Map.Entry<Boolean, JsonNode> parseSettingsFile() {
-        try {
-            JsonNode settings = MAPPER.readTree(new File(SETTINGS));
-            return new AbstractMap.SimpleEntry<>(settings.has(DDRAGON_VERSION) && settings.has(LANGUAGE), settings);
-        } catch (IOException ignored) {
-            return new AbstractMap.SimpleEntry<>(false, null);
-        }
-    }
+    protected abstract boolean shouldReloadData();
 }
