0d2ab65f46e345010f0ede6b87a251fa35171e3f
Petersil1998 <silbernagl.peter@rolmail.net>
2023-07-12 10:10:38 +0200

    Added LoR AppType and added LoR Ranked Endpoint

--- src/main/java/net/petersil98/core/http/RiotAPI.java
+++ src/main/java/net/petersil98/core/http/RiotAPI.java
@@ -44,6 +44,7 @@ public class RiotAPI {
     private static final String ACCOUNT_V1 = "account/v1/";
     private static final String LEAGUE_V1 = "league/v1/";
     private static final String MATCH_V1 = "match/v1/";
+    private static final String RANKED_V1 = "ranked/v1/";
 
     public static RateLimiter rateLimiter = new BlockingRateLimiter();
 
@@ -582,6 +583,72 @@ public class RiotAPI {
     }
 
     /**
+     * Requests the LoR {@link RiotAPI#RANKED_V1} endpoint. If successful, the Response is mapped to the desired Class <b>T</b>.
+     * If caching is enabled, the cached response will be returned.
+     * @see Settings#useCache(boolean)
+     * @param method Method in the Endpoint that should get called
+     * @param args Extra data needed for the Request
+     * @param region Region to make the request to
+     * @param requiredClass Class to which the response should get mapped to
+     * @return An object of class <b>T</b> if casting is successful, {@code null} otherwise
+     */
+    public static <T> T requestLorRankedEndpoint(String method, String args, Region region, Class<T> requiredClass) {
+        return requestLorRankedEndpoint(method, args, region, requiredClass, new HashMap<>());
+    }
+
+    /**
+     * Requests the LoR {@link RiotAPI#RANKED_V1} endpoint. If successful, the Response is mapped to the desired {@link TypeBase}.
+     * This method is intended to be used for {@link com.fasterxml.jackson.databind.type.CollectionType CollectionTypes} or
+     * {@link com.fasterxml.jackson.databind.type.MapType MapTypes}.
+     * If caching is enabled, the cached response will be returned.
+     * @see Settings#useCache(boolean)
+     * @see TypeFactory
+     * @param method Method in the Endpoint that should get called
+     * @param args Extra data needed for the Request
+     * @param region Region to make the request to
+     * @param requiredClass Class to which the response should get mapped to
+     * @return An object of Type <b>{@code requiredClass}</b> if casting is successful, {@code null} otherwise
+     */
+    public static <T> T requestLorRankedEndpoint(String method, String args, Region region, TypeBase requiredClass) {
+        return requestLorRankedEndpoint(method, args, region, requiredClass, new HashMap<>());
+    }
+
+    /**
+     * Requests the LoR {@link RiotAPI#RANKED_V1} endpoint. If successful, the Response is mapped to the desired Class <b>T</b>.
+     * If caching is enabled, the cached response will be returned.
+     * @see Settings#useCache(boolean)
+     * @param method Method in the Endpoint that should get called
+     * @param args Extra data needed for the Request
+     * @param region Region to make the request to
+     * @param requiredClass Class to which the response should get mapped to
+     * @param filter The filter that should get used for the request. <b>Note:</b> The Values in the Map need to be Strings,
+     *               even if they represent an integer
+     * @return An object of class <b>T</b> if casting is successful, {@code null} otherwise
+     */
+    public static <T> T requestLorRankedEndpoint(String method, String args, Region region, Class<T> requiredClass, Map<String, String> filter) {
+        return requestLorRankedEndpoint(method, args, region, TypeFactory.defaultInstance().constructType(requiredClass), filter);
+    }
+
+    /**
+     * Requests the LoR {@link RiotAPI#RANKED_V1} endpoint. If successful, the Response is mapped to the desired {@link JavaType} <b>{@code requiredClass}</b>.
+     * If caching is enabled, the cached response will be returned.
+     * @see Settings#useCache(boolean)
+     * @see TypeFactory
+     * @param method Method in the Endpoint that should get called
+     * @param args Extra data needed for the Request
+     * @param region Region to make the request to
+     * @param requiredClass Class to which the response should get mapped to
+     * @param filter The filter that should get used for the request. <b>Note:</b> The Values in the Map need to be Strings,
+     *               even if they represent an integer
+     * @return An object of Type <b>{@code requiredClass}</b> if casting is successful, {@code null} otherwise
+     */
+    public static <T> T requestLorRankedEndpoint(String method, String args, Region region, JavaType requiredClass, Map<String, String> filter) {
+        return handleCacheAndRateLimiter(
+                constructUrl(RANKED_V1 + method + args, AppType.LOR, region),
+                RANKED_V1 + method, region, requiredClass, filter);
+    }
+
+    /**
      * Utility Method that reads the cached response if caching is enable and that deals with the Rate Limiter (blocking) while making the API request
      * @param fullUrl The full url for the request
      * @param endpointMethod The Endpoint used in the url. Used for the Rate Limiter
@@ -667,7 +734,8 @@ public class RiotAPI {
     private enum AppType {
         RIOT("riot"),
         LOL("lol"),
-        TFT("tft");
+        TFT("tft"),
+        LOR("lor");
 
         private final String name;
 
