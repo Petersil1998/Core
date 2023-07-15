cb0e5539babc92237d8b15d3d9933b819226f181
Petersil1998 <silbernagl.peter@rolmail.net>
2023-07-15 11:01:11 +0200

    Added variable to check whether or not a language is available in the Valorant API

--- src/main/java/net/petersil98/core/util/settings/Language.java
+++ src/main/java/net/petersil98/core/util/settings/Language.java
@@ -4,48 +4,50 @@ package net.petersil98.core.util.settings;
  * Enumeration of all possible Languages supported by Data Dragon
  */
 public enum Language {
-    AR_AE("ar_AE"),
-    CS_CZ("cs_CZ"),
-    DE_DE("de_DE", true),
-    EL_GR("el_GR"),
-    EN_AU("en_AU"),
-    EN_GB("en_GB"),
-    EN_PH("en_PH"),
-    EN_SG("en_SG"),
-    EN_US("en_US", true),
-    ES_AR("es_AR"),
-    ES_ES("es_ES", true),
-    ES_MX("es_MX", true),
-    FR_FR("fr_FR", true),
-    HU_HU("hu_HU"),
-    IT_IT("it_IT", true),
-    JA_JP("ja_JP", true),
-    KO_KR("ko_KR", true),
-    PL_PL("pl_PL", true),
-    PT_BR("pt_BR", true),
-    RO_RO("ro_RO"),
-    RU_RU("ru_RU", true),
-    TH_TH("th_TH", true),
-    TR_TR("tr_TR", true),
-    VI_VN("vi_VN"),
-    ZH_CN("zh_CN"),
-    ZH_MY("zh_MY"),
-    ZH_TW("zh_TW", true);
+    AR_AE("ar_AE", false, true),
+    CS_CZ("cs_CZ", false, false),
+    DE_DE("de_DE", true, true),
+    EL_GR("el_GR", false, false),
+    EN_AU("en_AU", false, false),
+    EN_GB("en_GB", false, false),
+    EN_PH("en_PH", false, false),
+    EN_SG("en_SG", false, false),
+    EN_US("en_US", true, true),
+    ES_AR("es_AR", false, false),
+    ES_ES("es_ES", true, true),
+    ES_MX("es_MX", true, true),
+    FR_FR("fr_FR", true, true),
+    HU_HU("hu_HU", false, false),
+    IT_IT("it_IT", true, true),
+    JA_JP("ja_JP", true, true),
+    KO_KR("ko_KR", true, true),
+    PL_PL("pl_PL", true, true),
+    PT_BR("pt_BR", true, true),
+    RO_RO("ro_RO", false, false),
+    RU_RU("ru_RU", true, true),
+    TH_TH("th_TH", true, true),
+    TR_TR("tr_TR", true, true),
+    VI_VN("vi_VN", false, true),
+    ZH_CN("zh_CN", false, true),
+    ZH_MY("zh_MY", false, false),
+    ZH_TW("zh_TW", true, true);
 
     final String name;
     final boolean availableForLor;
+    final boolean availableForVal;
 
-    Language(String name) {
-        this(name, false);
-    }
-
-    Language(String name, boolean availableForLor) {
+    Language(String name, boolean availableForLor, boolean availableForVal) {
         this.name = name;
         this.availableForLor = availableForLor;
+        this.availableForVal = availableForVal;
     }
 
     public boolean isAvailableForLor() {
         return availableForLor;
+    }
+
+    public boolean isAvailableForVal() {
+        return availableForVal;
     }
 
     @Override
